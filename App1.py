c1833165dd237b17feb0fcbaabff6b3477d53d5e
Mahesh-Randale <82958249+Mahesh-Randale@users.noreply.github.com>
2021-04-23 12:57:33 +0530

    Update App1.py

--- App1.py
+++ App1.py
@@ -18,7 +18,7 @@ from streamlit_webrtc import (
 
 
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
-    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19702"]}]},
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
     media_stream_constraints={"video": True, "audio": True},
 )
 
